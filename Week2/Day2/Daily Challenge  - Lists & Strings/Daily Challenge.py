5bce59ef8fe59582f270a6a310f5593741034d6b
Mike <mt.mail.mt@gmail.com>
2023-05-08 23:05:29 +0300

    Commiting Daily Challenge

---
+++ Week2/Day2/Daily Challenge  - Lists & Strings/Daily Challenge.py
@@ -0,0 +1,32 @@
+
+
+# CHALLENGE 1
+# number = int(input("Give me a number "))
+# length = int(input("Give me a length "))
+# result = []
+# for i in range(1, length+1):
+#     result.append(number * i)
+
+#     print(result)
+
+
+# CHALLENGE 2
+# print("Enter a String: ")
+# str = input()
+
+# strlist = []
+# for i in range(len(str)):
+#     strlist.append(str[i])
+
+# newlist = []
+# j = 0
+# for i in range(len(strlist)):
+#     if str[i] not in newlist:
+#         newlist.insert(j, str[i])
+#         j = j+1
+
+# print("\nOriginal String:", str)
+
+# str = ""
+# str = str.join(newlist)
+# print("New String:", str)
