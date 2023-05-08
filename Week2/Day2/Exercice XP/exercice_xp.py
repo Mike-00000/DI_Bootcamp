e19d57360f9b15cf4ded1a4e2202843ef710ca94
Mike <mt.mail.mt@gmail.com>
2023-05-08 18:26:36 +0300

    Commiting ExercicesXP (from week2day2)

---
+++ Week2/Day2/Exercice XP/exercice_xp.py
@@ -0,0 +1,110 @@
+# Exercise 1 : Set
+# my_fav_numbers = {1, 5, 12, 18, 26, 69}
+# print(my_fav_numbers)
+# my_fav_numbers.add(99)
+# my_fav_numbers.add(1024)
+# print(my_fav_numbers)
+# my_fav_numbers.discard(1024)
+# print(my_fav_numbers)
+
+# friend_fav_numbers = {6, 66, 666}
+
+# our_fav_numbers = my_fav_numbers.union(friend_fav_numbers)
+# print(our_fav_numbers)
+
+
+# Exercise 2: Tuple
+# NO
+
+# Exercise 3: List
+# basket = ["Banana", "Apples", "Oranges", "Blueberries"]
+# print(basket)
+# basket.remove("Banana")
+# print(basket)
+# basket.remove("Blueberries")
+# print(basket)
+# basket.append("kiwi")
+# print(basket)
+# basket.insert(0,"Apples")
+# print(basket)
+# basket.clear()
+# print(basket)
+
+
+# Exercise 4: Floats
+
+
+# Exercise 5: For Loop
+# for i in range(0, 21): 
+# 	print(i)
+
+# for i in range(0, 21, 2): 
+# 	print(i)
+
+
+# Exercise 6 : While Loop
+# name = ""
+# while name != "Michael":
+#     name = input("What is your name? ")
+# print("Ok, it's the good name!")
+
+
+# Exercise 7: Favorite Fruits
+# fav_fruits = input("What's your favorite fruits? (separate the fruits with a single space) ")
+# string2list = fav_fruits.split()
+# input("Enter a name of any fruit ")
+# if fav_fruits in string2list:
+#     print("You chose one of your favorite fruits! Enjoy")
+# else:
+#         print("You chose a new fruit. I hope you enjoy")
+
+
+# Exercise 8: Who Ordered A Pizza ?
+# while True:
+#     topping = input("What topping do you want on your pizza (type 'quit' if it is enough)? ")
+#     if topping != "quit":
+#         print(f"We will add {topping} to your pizza ")
+#     else:
+#         print("Great! Now let us prepare your pizza :) ")
+#         break
+
+
+# Exercise 9: Cinemax
+# PART 1
+# total = 0
+# num_members = int(input("Enter the number of family members: "))
+# for i in range(num_members):
+#     age = int(input("Enter family member's age: "))
+#     if age < 3:
+#         total += 0
+#     elif 3 < age <= 12:
+#         total += 10
+#     else:
+#         total += 15
+# print(f"The price is {total} dollars")
+
+# PART 2
+# teenagers = ["Tom", "Alice", "Rick", "Richard", "Robert"]
+# new_teens = []
+# for i in teenagers:
+#     print(i)
+#     teen_age = int(input("What's you age? "))
+#     if 16 <= teen_age <= 21:
+#         new_teens.append(i)
+
+# print(new_teens)
+
+
+
+
+# Exercise 10 : Sandwich Orders
+# sandwich_orders = ["Tuna sandwich", "Avocado sandwich", "Egg sandwich", "Sabih sandwich", "Pastrami sandwich"]
+# finished_sandwiches = []
+# for i in sandwich_orders:
+#     sandwich_orders.remove(i)
+#     finished_sandwiches.append(i)
+# print(f"I made {finished_sandwiches}")
+
+
+# Exercise 11 : Sandwich Orders#2
+
