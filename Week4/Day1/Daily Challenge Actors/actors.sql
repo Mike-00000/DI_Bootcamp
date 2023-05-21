4091d801e3ad99a8f289706b72d58d13d644be56
Mike <mt.mail.mt@gmail.com>
2023-05-21 16:01:00 +0300

    Commiting W4D1 Daily Challenge Actors

---
+++ Week4/Day1/Daily Challenge Actors/actors.sql
@@ -0,0 +1,11 @@
+
+-- SELECT COUNT(actor_id) FROM actor;
+
+
+-- INSERT INTO actor (first_name, last_name, age, num_oscars)
+-- VALUES ('Arnold', '', '', 2)
+
+-- INSERT INTO actor (first_name, last_name, age, num_oscars)
+-- VALUES ('Arnold', 'Schwarzenegger', '1947-07-30', 2)
+
+SELECT * FROM actor
