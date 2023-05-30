3cb12eff5aa6edf70ea521e3fe3826102171cc85
Mike <mt.mail.mt@gmail.com>
2023-05-30 13:51:51 +0300

    Commiting W5D2 Exercice XP

---
+++ Week5/Day2/Exercice XP/animal-info/info/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Family(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Animal(models.Model):
+    legs = models.IntegerField()
+    weight = models.FloatField()
+    height = models.FloatField()
+    speed = models.FloatField()
+    family = models.ForeignKey(Family, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Animal ID: {self.id}"
---
+++ Week5/Day2/Exercice XP/animals.sql
@@ -0,0 +1,6 @@
+-- INSERT INTO info_animal (legs, weight, height, speed, family_id) VALUES (4, 200, 100, 50, 1);
+-- INSERT INTO info_animal (legs, weight, height, speed, family_id) VALUES (8, 0.1, 1, 02, 4);
+
+-- INSERT INTO info_animal (legs, weight, height, speed, family_id) VALUES (4, 50, 40, 5, 2);
+
+SELECT * FROM info_animal
