be427d3ec2c8e448a5124b6f4e369422cde99376
Mike <mt.mail.mt@gmail.com>
2023-06-16 12:22:11 +0300

    Commiting W5 D5 DC

--- Week5/Day2/Daily Challenge/phonebook/phonebookapp/admin.py
+++ Week5/Day2/Daily Challenge/phonebook/phonebookapp/admin.py
@@ -1,9 +1,28 @@
 from django.contrib import admin
-from .models import Person, Post, Category, Email, Address
+from django import forms
+from phonenumber_field.formfields import PhoneNumberField
+from phonenumber_field.widgets import PhoneNumberInternationalFallbackWidget
+from .models import Person, Post, Category, Email, Address, PersonAdmin
 # Register your models here.
 
-admin.site.register(Person)
+
+class PersonForm(forms.ModelForm):
+    phone_number = PhoneNumberField(widget=PhoneNumberInternationalFallbackWidget)
+
+    class Meta:
+        model = Person
+        fields = '__all__'
+
+class PersonAdmin(admin.ModelAdmin):
+    form = PersonForm
+    list_display = ('name', 'email', 'address')
+    list_display_links = ('name', 'email', 'address')
+    search_fields = ('name', 'email', 'phone_number')
+    exclude = ('id',)
+
+admin.site.register(Person, PersonAdmin)
 admin.site.register(Post)
 admin.site.register(Category)
 admin.site.register(Email)
 admin.site.register(Address)
+
