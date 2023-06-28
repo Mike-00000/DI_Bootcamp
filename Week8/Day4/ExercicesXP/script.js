0db281da07e2dae42d4a02523a992567c9bed77b
Mike <mt.mail.mt@gmail.com>
2023-06-28 16:21:07 +0300

    Commiting W8 D4 XP

---
+++ Week8/Day4/ExercicesXP/script.js
@@ -0,0 +1,89 @@
+
+// EXERCICES XP
+
+// _______________________________________________
+// Exercise 1 : Location
+// "I am John Doe from Vancouver, Canada. Latitude(49.2827), Longitude(-123.1207)"
+
+
+
+// _______________________________________________
+// Exercise 2: Display Student Info
+// function displayStudentInfo(objUser){
+//     let {first, last} = objUser
+//     return `Your full name is ${first} ${last}`;
+// }
+
+// console.log(displayStudentInfo({first: 'Elie', last:'Schoppik'}));
+
+
+
+// _______________________________________________
+// Exercise 3: User & Id
+// const users = { user1: 18273, user2: 92833, user3: 90315 };
+
+// const userArray = Object.entries(users);
+
+// console.log(userArray);
+
+// const x2UserArray = userArray.map(([key, value]) => [key, value * 2]);
+
+// console.log(x2UserArray);
+
+
+
+// _______________________________________________
+// Exercise 4 : Person Class
+// "Object"
+
+
+
+// _______________________________________________
+// Exercise 5 : Dog Class
+// "The good answer is the second:"
+
+// class Labrador extends Dog {
+//     constructor(name, size) {
+//       super(name);
+//       this.size = size;
+//     }
+//   };
+
+// _______________________________________________
+// Exercise 6 : Challenges
+
+// 6.1 True or False ?
+// True
+// False
+
+
+// 6.2 value property number 
+
+
+// 6.2.1
+// class Animal {
+//     constructor (name, type, color) {
+//         this.name = name
+//         this.type = type
+//         this.color = color
+//     }
+// }
+
+
+// // 6.2.2
+// class Mamal extends Animal {
+    
+//     sound (sound) {
+//         return `Moooo, I'm a ${this.type}, named ${this.name} and I'm ${this.color}. ${sound}`; 
+//     }
+// }
+
+
+// // 6.2.3
+// const farmerCow = new Mamal('Lily', 'cow', 'brown and white');
+// console.log(farmerCow.sound('Moo'));
+
+
+
+
+
