321c5b990e1b81545e033e5c936c322206f88db6
Mike <mt.mail.mt@gmail.com>
2023-07-09 14:48:19 +0300

    Commiting W10 D1 XP

---
+++ Week10/Day1/ExerciceXP/npm-node/main.js
@@ -0,0 +1,29 @@
+
+// EXERCICE XP
+
+// ________________________________________________________
+// Exercise 1 : Hello Node.Js
+
+// const a = 5;
+// const b = 10;
+// const sum = a + b
+// console.log("Hello, you are " + sum);
+
+
+// ________________________________________________________
+// Exercise 2 : Hello Node.Js #2
+
+// console.log("Hi! How are you");
+
+
+
+// ________________________________________________________
+// Exercise 3 : Nodemon
+const a = 5;
+const b = 10;
+let x;
+const c = 6;
+x = a + b;
+const sum = x + c
+console.log(sum);
+
